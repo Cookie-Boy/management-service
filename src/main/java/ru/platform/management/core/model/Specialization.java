d722c8e9e86c27059de706aae022aeab31fac6e0
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2026-05-19 21:01:14 +0700

    Clean Specialization enum

--- src/main/java/ru/platform/management/core/model/Specialization.java
+++ src/main/java/ru/platform/management/core/model/Specialization.java
@@ -1,35 +1,14 @@
 package ru.platform.management.core.model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-
-@Getter
 public enum Specialization {
-    GENERAL_PRACTITIONER("Терапевт"),
-    SURGEON("Хирург"),
-    DENTIST("Стоматолог"),
-    DERMATOLOGIST("Дерматолог"),
-    OPHTHALMOLOGIST("Офтальмолог"),
-    CARDIOLOGIST("Кардиолог"),
-    ONCOLOGIST("Онколог"),
-    EXOTIC_PETS("Экзотические животные"),
-    FELINE("Кошки"),
-    CANINE("Собаки");
-
-    private final String name;
-
-    Specialization(String name) {
-        this.name = name;
-    }
-
-    @JsonCreator
-    public static Specialization fromName(String name) {
-        for (Specialization specialization : Specialization.values()) {
-            if (specialization.name.equalsIgnoreCase(name)) {
-                return specialization;
-            }
-        }
-        throw new IllegalArgumentException("Неизвестная специализация: " + name);
-    }
+    GENERAL_PRACTITIONER,
+    SURGEON,
+    DENTIST,
+    DERMATOLOGIST,
+    OPHTHALMOLOGIST,
+    CARDIOLOGIST,
+    ONCOLOGIST,
+    EXOTIC_PETS,
+    FELINE,
+    CANINE
 }
